d13d51982e7fb7a0998fd4c8205aff137278f3da
Christopher Aue <mail@christopheraue.net>
2020-11-01 12:26:32 +0100

    Store depth buffer in texture if WEBGL_depth_texture is available

--- src/components/App/Canvas/instance.js
+++ src/components/App/Canvas/instance.js
@@ -27,17 +27,32 @@ function Pass(webGL, key, config, renderToCanvas = false) {
 
   this.framebuffer = this.webGL.createFramebuffer()
   this.attachments = {color: this.webGL.createTexture()}
-  this.depthBuffer = this.webGL.createRenderbuffer()
+
+  if (this.webGL.getExtension('WEBGL_depth_texture')) {
+    this.attachments.depth = this.webGL.createTexture()
+  } else {
+    this.depthBuffer = this.webGL.createRenderbuffer()
+  }
 
   this.webGL.bindFramebuffer(this.webGL.FRAMEBUFFER, this.framebuffer)
+
   this.webGL.activeTexture(this.webGL.TEXTURE0)
   this.webGL.bindTexture(this.webGL.TEXTURE_2D, this.attachments.color)
-  this.webGL.bindRenderbuffer(this.webGL.RENDERBUFFER, this.depthBuffer)
   this.webGL.framebufferTexture2D(this.webGL.FRAMEBUFFER, this.webGL.COLOR_ATTACHMENT0, this.webGL.TEXTURE_2D, this.attachments.color, 0)
-  this.webGL.framebufferRenderbuffer(this.webGL.FRAMEBUFFER, this.webGL.DEPTH_ATTACHMENT, this.webGL.RENDERBUFFER, this.depthBuffer)
-  this.webGL.bindFramebuffer(this.webGL.FRAMEBUFFER, null)
   this.webGL.bindTexture(this.webGL.TEXTURE_2D, null)
-  this.webGL.bindRenderbuffer(this.webGL.RENDERBUFFER, null)
+
+  if (this.attachments.depth) {
+    this.webGL.activeTexture(this.webGL.TEXTURE0)
+    this.webGL.bindTexture(this.webGL.TEXTURE_2D, this.attachments.depth)
+    this.webGL.framebufferTexture2D(this.webGL.FRAMEBUFFER, this.webGL.DEPTH_ATTACHMENT, this.webGL.TEXTURE_2D, this.attachments.depth, 0)
+    this.webGL.bindTexture(this.webGL.TEXTURE_2D, null)
+  } else {
+    this.webGL.bindRenderbuffer(this.webGL.RENDERBUFFER, this.depthBuffer)
+    this.webGL.framebufferRenderbuffer(this.webGL.FRAMEBUFFER, this.webGL.DEPTH_ATTACHMENT, this.webGL.RENDERBUFFER, this.depthBuffer)
+    this.webGL.bindRenderbuffer(this.webGL.RENDERBUFFER, null)
+  }
+
+  this.webGL.bindFramebuffer(this.webGL.FRAMEBUFFER, null)
 }
 
 Pass.prototype = {
@@ -53,9 +68,20 @@ Pass.prototype = {
     this.webGL.texParameteri(this.webGL.TEXTURE_2D, this.webGL.TEXTURE_WRAP_T, this.webGL.CLAMP_TO_EDGE)
     this.webGL.bindTexture(this.webGL.TEXTURE_2D, null)
 
-    this.webGL.bindRenderbuffer(this.webGL.RENDERBUFFER, this.depthBuffer)
-    this.webGL.renderbufferStorage(this.webGL.RENDERBUFFER, this.webGL.DEPTH_COMPONENT16, width, height)
-    this.webGL.bindRenderbuffer(this.webGL.RENDERBUFFER, null)
+    if (this.attachments.depth) {
+      this.webGL.activeTexture(this.webGL.TEXTURE0)
+      this.webGL.bindTexture(this.webGL.TEXTURE_2D, this.attachments.depth)
+      this.webGL.texImage2D(this.webGL.TEXTURE_2D, 0, this.webGL.DEPTH_COMPONENT, width, height, 0,
+                            this.webGL.DEPTH_COMPONENT, this.webGL.UNSIGNED_SHORT, null)
+      this.webGL.texParameteri(this.webGL.TEXTURE_2D, this.webGL.TEXTURE_MIN_FILTER, this.webGL.LINEAR)
+      this.webGL.texParameteri(this.webGL.TEXTURE_2D, this.webGL.TEXTURE_WRAP_S, this.webGL.CLAMP_TO_EDGE)
+      this.webGL.texParameteri(this.webGL.TEXTURE_2D, this.webGL.TEXTURE_WRAP_T, this.webGL.CLAMP_TO_EDGE)
+      this.webGL.bindTexture(this.webGL.TEXTURE_2D, null)
+    } else {
+      this.webGL.bindRenderbuffer(this.webGL.RENDERBUFFER, this.depthBuffer)
+      this.webGL.renderbufferStorage(this.webGL.RENDERBUFFER, this.webGL.DEPTH_COMPONENT16, width, height)
+      this.webGL.bindRenderbuffer(this.webGL.RENDERBUFFER, null)
+    }
   },
 
   updateShader({type, source, linked}) {
