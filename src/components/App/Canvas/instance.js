db3a5c07a9bca9cef15b6c8b0446a9b580f64fa2
Christopher Aue <mail@christopheraue.net>
2020-11-03 10:34:49 +0100

    Fix render artifacts caused by not enough bits for the depth buffer

--- src/components/App/Canvas/instance.js
+++ src/components/App/Canvas/instance.js
@@ -77,7 +77,7 @@ Pass.prototype = {
       this.webGL.activeTexture(this.webGL.TEXTURE0)
       this.webGL.bindTexture(this.webGL.TEXTURE_2D, this.attachments.depth)
       this.webGL.texImage2D(this.webGL.TEXTURE_2D, 0, this.webGL.DEPTH_COMPONENT, width, height, 0,
-                            this.webGL.DEPTH_COMPONENT, this.webGL.UNSIGNED_SHORT, null)
+                            this.webGL.DEPTH_COMPONENT, this.webGL.UNSIGNED_INT, null)
       this.webGL.texParameteri(this.webGL.TEXTURE_2D, this.webGL.TEXTURE_MIN_FILTER, this.webGL.LINEAR)
       this.webGL.texParameteri(this.webGL.TEXTURE_2D, this.webGL.TEXTURE_WRAP_S, this.webGL.CLAMP_TO_EDGE)
       this.webGL.texParameteri(this.webGL.TEXTURE_2D, this.webGL.TEXTURE_WRAP_T, this.webGL.CLAMP_TO_EDGE)
