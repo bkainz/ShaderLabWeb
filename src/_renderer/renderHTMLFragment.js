72856a6ac3a6c6c31a11d67c6fdae24969729110
Christopher Aue <mail@christopheraue.net>
2021-07-14 02:15:00 +0200

    Fix rendering html fragments after restarting the app

--- src/_renderer/renderHTMLFragment.js
+++ src/_renderer/renderHTMLFragment.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDomServer from 'react-dom/server'
 import './Component'
+import ComponentRegistry from './ComponentRegistry'
 
 export default function(Content) {
   return async function(props = {}) {
+    global.componentRegistry = new ComponentRegistry()
     return ReactDomServer.renderToStaticMarkup(<Content {...props}/>)
   }
 }
