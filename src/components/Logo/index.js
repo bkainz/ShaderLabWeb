595472421fe5210f062ff94fd858a7571472be99
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2022-04-01 17:53:39 +0200

    Added TUM logo.

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -5,5 +5,6 @@ export default Component.register(import.meta.url, ({className, id}) =>
     <h1 className={className+'-Title'}>ShaderLabWeb</h1>
     <div className={className+'-VerticalBar'}/>
     <img className={className+'-Logo'} src="/assets/logo.png"/>
+    <p style={{marginLeft: '0.3em'}}><img className={className+'-Logo'} src="/assets/logo_tum.png"/></p>
   </div>
 )
