dc35059ede908db7e0cef6b827a84eec619d60ed
bkainz <bkainz@imperial.ac.uk>
2025-08-03 12:39:02 +0100

    update logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -4,7 +4,7 @@ export default Component.register(import.meta.url, ({className, id}) =>
   <div className={className} id={id}>
     <h1 className={className+'-Title'}>ShaderLabWeb</h1>
     <div className={className+'-VerticalBar'}/>
-    <img className={className+'-Logo'} src="/assets/logo.png"/>
+    <img className={className+'-Logo'} src="/assets/logo_white.svg"/>
     <p style={{marginLeft: '0.3em'}}><img className={className+'-Logo'} src="/assets/logo_tum.png"/></p>
   </div>
 )
